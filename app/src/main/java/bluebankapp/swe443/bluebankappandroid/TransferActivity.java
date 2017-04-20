426aed1de706e7e0e864a59f93b5a3e111132da8
sjammi <sushenajammi@gmail.com>
2017-04-20 10:23:58 -0400

    Added a log out button

--- app/src/main/java/bluebankapp/swe443/bluebankappandroid/TransferActivity.java
+++ app/src/main/java/bluebankapp/swe443/bluebankappandroid/TransferActivity.java
@@ -1,14 +1,44 @@
 package bluebankapp.swe443.bluebankappandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import bluebankapp.swe443.bluebankappandroid.myapplication.resource.Account;
+import bluebankapp.swe443.bluebankappandroid.myapplication.resource.Bank;
+import bluebankapp.swe443.bluebankappandroid.myapplication.resource.User;
 
 public class TransferActivity extends AppCompatActivity {
 
+    Button submitBtn;
+    TextView amountText;
+    Bank blue;
+    Account current_acct;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        blue = (Bank) getIntent().getParcelableExtra("bank");
+        current_acct = (Account) getIntent().getParcelableExtra("current_acct");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+        submitBtn=(Button) findViewById(R.id.submitBtn);
+        //gets balances and prints it out on screen
+        amountText = (TextView) findViewById(R.id.currentBalanceTxt);
+        if(blue!=null){
+            bindBalance();
+        }else{
+            Toast.makeText(this, "No Bank", Toast.LENGTH_LONG).show();
+        }
     }
 
+
+
+    public void bindBalance(){
+        amountText.setText("Balance: $" + Double.toString(current_acct.getAccountBalance()));
+    }
 }

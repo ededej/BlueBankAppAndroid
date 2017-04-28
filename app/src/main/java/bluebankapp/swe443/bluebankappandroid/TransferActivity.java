786c24ee241c0f69e7fa8a94a70fd0a01e9101b4
Heymond Truong <htruong2@gmu.edu>
2017-04-27 20:58:24 -0400

    QR modification

--- app/src/main/java/bluebankapp/swe443/bluebankappandroid/TransferActivity.java
+++ app/src/main/java/bluebankapp/swe443/bluebankappandroid/TransferActivity.java
@@ -38,6 +38,8 @@ public class TransferActivity extends AppCompatActivity implements ZXingScannerV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+        //personNameEdit.setText(getIntent().getExtras().getString("user"));
+
     }
 
     public void doTransactionClick(View v){
@@ -99,8 +101,14 @@ public class TransferActivity extends AppCompatActivity implements ZXingScannerV
                     public void onClick(DialogInterface dialog, int id)
                     {
                         dialog.cancel();
-                        startActivity(new Intent(getBaseContext(), BankMainActivity.class));
-                        finish();
+                        setContentView(R.layout.activity_transfer);
+                        EditText u = (EditText) findViewById(R.id.personNameEdit);
+                        u.setText(rawResult.getText());
+                        //Intent intent = new Intent(getBaseContext(), TransferActivity.class);
+                        //intent.putExtra("user",rawResult.getText().toString());
+                        //startActivity(intent);
+                        //startActivity(new Intent(getBaseContext(), BankMainActivity.class));
+                        //finish();
                     }
                 });
         AlertDialog alert1 = builder.create();
@@ -108,70 +116,6 @@ public class TransferActivity extends AppCompatActivity implements ZXingScannerV
 
         // If you would like to resume scanning, call this method below:
         // mScannerView.resumeCameraPreview(this);
-    }
-
-
-//
-
-    private Bitmap generateQR(String s) throws WriterException{
-        BitMatrix result;
-        int width=700;
-        int height=700;
-        try {
-            result = new MultiFormatWriter().encode(s, BarcodeFormat.QR_CODE, width, height, null);
-        } catch (IllegalArgumentException iae) {
-            // Unsupported format
-            return null;
-        }
-        int w = result.getWidth();
-        int h = result.getHeight();
-        int[] pixels = new int[w * h];
-        for (int y = 0; y < h; y++) {
-            int offset = y * w;
-            for (int x = 0; x < w; x++) {
-                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
-            }
-        }
-        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        bitmap.setPixels(pixels, 0, width, 0, 0, w, h);
-        return bitmap;
-    }
-
-    private void showAlertQr(String transactionId) {
-        ImageView imageForQr = new ImageView(this);
-        try {
-            imageForQr.setImageBitmap(generateQR(transactionId));
-        } catch (WriterException e) {
-            e.printStackTrace();
-        }
-
-        AlertDialog builder =
-                new AlertDialog.Builder(this).
-                        setNeutralButton("Done", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.dismiss();
-                            }
-                            }).
-                        setView(imageForQr).create();
-        TextView title = new TextView(this);
-        title.setText("SCAN QR CODE ABOVE");
-        title.setTextColor(BLACK);
-        title.setTextSize(20);
-        title.setPadding(10, 10, 10, 10);
-        title.setGravity(Gravity.CENTER);
-        builder.setCustomTitle(title);
-        //trying to put the done button on center
-        builder.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {                    //
-                Button positiveButton = ((AlertDialog) dialog)
-                        .getButton(AlertDialog.BUTTON_POSITIVE);
-                positiveButton.setGravity(Gravity.CENTER_HORIZONTAL);
-                positiveButton.setTextSize(50);
-            }
-        });
-        builder.show();
-
     }
 
     public boolean validateInput(){

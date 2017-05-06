3c7962eec2c6072d88b4d3cf0d9db2f42d87f84e
ededej <ededej@masonlive.gmu.edu>
2017-05-06 00:22:13 -0400

    addes some checks for empty string on confirm button to not crash the app

--- app/src/main/java/bluebankapp/swe443/bluebankappandroid/SetFeeActivity.java
+++ app/src/main/java/bluebankapp/swe443/bluebankappandroid/SetFeeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SetFeeActivity extends AppCompatActivity {
 
@@ -34,39 +35,58 @@ public class SetFeeActivity extends AppCompatActivity {
 //    }
 
     public void doMakeChanges(View v){
-        Double dfee= Double.parseDouble(dfText.getText().toString());
-        Double wfee= Double.parseDouble(wfText.getText().toString());
-        Double tfee= Double.parseDouble(tfText.getText().toString());
-        String u = getSharedPreferences("bluebank", MODE_PRIVATE).getString("username", "");
-        String p = getSharedPreferences("bluebank", MODE_PRIVATE).getString("password", "");
-        String ip = getSharedPreferences("bluebank", MODE_PRIVATE).getString("ip", "");
+        Double dfee=0.0;
+        Double wfee=0.0;
+        Double tfee=0.0;
+        boolean check=false;
+        if(!(dfText.getText().toString().isEmpty())){
+             dfee= Double.parseDouble(dfText.getText().toString());
+            check=true;
+        }
+        if(!(wfText.getText().toString().isEmpty())){
+             wfee= Double.parseDouble(wfText.getText().toString());
+            check=true;
 
-        //Send REFRESH request to server.
-        StringBuilder req = new StringBuilder();
+        }
+        if(!(tfText.getText().toString().isEmpty())){
+             tfee= Double.parseDouble(tfText.getText().toString());
+             check=true;
+        }
+        if(check) {
+            String u = getSharedPreferences("bluebank", MODE_PRIVATE).getString("username", "");
+            String p = getSharedPreferences("bluebank", MODE_PRIVATE).getString("password", "");
+            String ip = getSharedPreferences("bluebank", MODE_PRIVATE).getString("ip", "");
 
-        // Create the request string
-        // op code | username | password | amount
-        // 0  #1  #2       #3
-        // r#jlm#letmein0#50.00
+            //Send REFRESH request to server.
+            StringBuilder req = new StringBuilder();
+
+            // Create the request string
+            // op code | username | password | amount
+            // 0  #1  #2       #3
+            // r#jlm#letmein0#50.00
 
 //        if(isAdmin == false){
 //            req.append("h" + ClientLogic.DELIM); // OP CODE
 //        } else {
 //            req.append("a" + ClientLogic.DELIM); // OP CODE
 //        }
-        req.append("f" + ClientLogic.DELIM);
-        req.append(u + ClientLogic.DELIM); // USERNAME
-        req.append(p); // PASSWORD
-        req.append(ClientLogic.DELIM);
-        req.append(Double.toString(dfee));
-        req.append(ClientLogic.DELIM);
-        req.append(Double.toString(wfee));
-        req.append(ClientLogic.DELIM);
-        req.append(Double.toString(tfee));
-        req.append(ClientLogic.DELIM);
+            req.append("f" + ClientLogic.DELIM);
+            req.append(u + ClientLogic.DELIM); // USERNAME
+            req.append(p); // PASSWORD
+            req.append(ClientLogic.DELIM);
+            req.append(Double.toString(dfee));
+            req.append(ClientLogic.DELIM);
+            req.append(Double.toString(wfee));
+            req.append(ClientLogic.DELIM);
+            req.append(Double.toString(tfee));
+            req.append(ClientLogic.DELIM);
 
 
-        // Send the request string and get the response.
-        new ClientLogic.TransactionRefreshRequest().execute(this, req.toString(), ip);
+            // Send the request string and get the response.
+            new ClientLogic.TransactionRefreshRequest().execute(this, req.toString(), ip);
+        }else{
+            Toast.makeText(this, "You need to change one of the fees before confirm!!!", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
